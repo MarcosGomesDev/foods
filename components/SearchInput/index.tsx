d0df114170442abbaed5c69fae222fc30cecdc58
Marcos Gomes <marcosgomesdev@gmail.com>
2024-05-28 05:49:59 -0300

    style: change background color on search input

--- components/SearchInput/index.tsx
+++ components/SearchInput/index.tsx
@@ -12,7 +12,7 @@ export function SearchInput({ className }: SearchInputProps) {
       <Input
         type="search"
         placeholder="Buscar restaurantes"
-        className="border-none bg-white"
+        className="border-none"
       />
       <Button size="icon">
         <SearchIcon size={20} />
