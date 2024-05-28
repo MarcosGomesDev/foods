b769787868173ac01f0e0b7429573e97021b93bd
Marcos Gomes <marcosgomesdev@gmail.com>
2024-05-28 06:22:29 -0300

    feat: add href prop link

--- components/ProductList/index.tsx
+++ components/ProductList/index.tsx
@@ -1,5 +1,6 @@
 import { Prisma } from "@prisma/client";
 import { ChevronRightIcon } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 import { twMerge } from "tailwind-merge";
 import { ProductItem } from "../ProductItem";
@@ -8,6 +9,7 @@ import { Button } from "../ui/button";
 interface ProductListProps extends React.HTMLAttributes<HTMLDivElement> {
   title?: string;
   hasButton?: boolean;
+  href?: string;
   products: Prisma.ProductGetPayload<{
     include: {
       restaurant: {
@@ -23,6 +25,7 @@ export function ProductList({
   className,
   title,
   hasButton = false,
+  href = "/products",
   products,
 }: ProductListProps) {
   return (
@@ -30,13 +33,15 @@ export function ProductList({
       <div className="flex items-center justify-between px-5">
         {title && <h2 className="font-semibold">{title}</h2>}
         {hasButton && (
-          <Button
-            variant="ghost"
-            className="h-fit p-0 text-primary hover:bg-transparent"
-          >
-            Ver todos
-            <ChevronRightIcon size={16} />
-          </Button>
+          <Link href={href}>
+            <Button
+              variant="ghost"
+              className="h-fit p-0 text-primary hover:bg-transparent"
+            >
+              Ver todos
+              <ChevronRightIcon size={16} />
+            </Button>
+          </Link>
         )}
       </div>
 
--- components/RestaurantList/index.tsx
+++ components/RestaurantList/index.tsx
@@ -9,6 +9,7 @@ interface RestaurantListProps extends React.HTMLAttributes<HTMLDivElement> {
   title?: string;
   hasButton?: boolean;
   vertical?: boolean;
+  href?: string;
   restaurants: Restaurant[];
 }
 
@@ -17,6 +18,7 @@ export function RestaurantList({
   title,
   hasButton = false,
   vertical = false,
+  href = "/restaurants",
   restaurants,
 }: RestaurantListProps) {
   return (
@@ -24,7 +26,7 @@ export function RestaurantList({
       <div className="flex items-center justify-between px-5">
         {title && <h2 className="font-semibold">{title}</h2>}
         {hasButton && (
-          <Link href="/restaurants/recommended">
+          <Link href={href}>
             <Button
               variant="ghost"
               className="h-fit p-0 text-primary hover:bg-transparent"
