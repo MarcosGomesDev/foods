e33e9023eba17652f947370aa38feae446f025ae
Marcos Gomes <marcosgomesdev@gmail.com>
2024-06-13 21:36:01 -0300

    feat: add link on PromoBanner component

--- components/PromoBanner/index.tsx
+++ components/PromoBanner/index.tsx
@@ -1,14 +1,22 @@
 import Image, { ImageProps } from "next/image";
+import Link, { LinkProps } from "next/link";
 
-export function PromoBanner(props: ImageProps) {
+interface PromoBannerProps extends ImageProps {
+  href: LinkProps["href"];
+}
+
+export function PromoBanner({ href, alt, ...props }: PromoBannerProps) {
   return (
-    <Image
-      width={0}
-      height={0}
-      className="h-auto w-full object-contain"
-      sizes="100vw"
-      quality={100}
-      {...props}
-    />
+    <Link href={href}>
+      <Image
+        width={0}
+        height={0}
+        className="h-auto w-full object-contain"
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        alt={alt}
+        quality={100}
+        {...props}
+      />
+    </Link>
   );
 }
