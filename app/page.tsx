a6cdac33e9ec11cf3b66f5304a778b6545522f3e
Marcos Gomes <marcosgomesdev@gmail.com>
2024-06-13 21:39:48 -0300

    feat: add desktop layout and desktop banners

--- app/page.tsx
+++ app/page.tsx
@@ -28,44 +28,88 @@ export default async function Home() {
   });
 
   return (
-    <div className="mb-3 pb-3">
+    <>
       <Header />
+      <div className="mb-12 pb-12">
+        <SearchInput className="px-5 pt-6 lg:hidden" />
 
-      <SearchInput className="px-5 pt-6" />
+        <div className="relative mt-6 hidden h-[500px] w-full justify-between overflow-hidden bg-red-600 px-28 lg:flex">
+          <div className="flex h-full flex-col justify-center space-y-4 text-left text-white">
+            <h3 className=" text-5xl font-bold ">Esta com fome?</h3>
+            <p>
+              Com apenas alguns cliques, encontre refeições acessíveis perto de
+              você.
+            </p>
+            <div className="flex h-[88px] w-[658px] items-center rounded-md bg-white p-5">
+              <SearchInput className="w-full" />
+            </div>
+          </div>
+          <div className="">
+            <div className="absolute bottom-0 right-36 drop-shadow-2xl">
+              <PromoBanner
+                href={"/"}
+                src="/banner-desktop.png"
+                alt="Até 30% de desconto em pizzas"
+                priority
+                className="h-auto w-[372px]"
+              />
+            </div>
+          </div>
+        </div>
 
-      <CategoryList className="px-5 pb-1 pt-6" />
+        <CategoryList className="px-5 pb-1 pt-6 lg:mt-10" />
 
-      <div className="px-5 pt-6">
-        <PromoBanner
-          src="/promo-banner-01.png"
-          alt="Até 30% de desconto em pizzas"
-          priority
+        <div className="px-5 pt-6 lg:hidden">
+          <PromoBanner
+            href={"/"}
+            src="/promo-banner-01.png"
+            alt="Até 30% de desconto em pizzas"
+            priority
+          />
+        </div>
+
+        <ProductList
+          className="space-y-4 pt-6 lg:mt-10"
+          title="Pedidos Recomendados"
+          hasButton
+          products={products}
+          href="/products/recommended"
+        />
+
+        <div className="px-5 pt-6 lg:hidden">
+          <PromoBanner
+            href={"/"}
+            src="/promo-banner-02.png"
+            alt="A partir de R$ 17,90 em lanches"
+            priority
+          />
+        </div>
+
+        <div className="container mt-10 hidden justify-center gap-4 pt-6 lg:flex">
+          <PromoBanner
+            href={"/"}
+            src="/promo-banner-01-desk.png"
+            alt="Até 30% de desconto em pizzas"
+            priority
+            className="w-[610px]"
+          />
+          <PromoBanner
+            href={"/"}
+            src="/promo-banner-02-desk.png"
+            alt="A partir de R$ 17,90 em lanches"
+            priority
+            className="w-[610px]"
+          />
+        </div>
+
+        <RestaurantList
+          className="space-y-4 py-6 lg:mt-10"
+          title="Restaurantes Recomendados"
+          hasButton
+          restaurants={restaurants}
+          href="/restaurants/recommended"
         />
       </div>
-
-      <ProductList
-        className="space-y-4 pt-6"
-        title="Pedidos Recomendados"
-        hasButton
-        products={products}
-        href="/products/recommended"
-      />
-
-      <div className="px-5 pt-6">
-        <PromoBanner
-          src="/promo-banner-02.png"
-          alt="A partir de R$ 17,90 em lanches"
-          priority
-        />
-      </div>
-
-      <RestaurantList
-        className="space-y-4 py-6"
-        title="Restaurantes Recomendados"
-        hasButton
-        restaurants={restaurants}
-        href="/restaurants/recommended"
-      />
-    </div>
+    </>
   );
 }
