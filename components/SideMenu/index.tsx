25d0c9ac0d497802cb7c97401b60e257980aa95b
Marcos Gomes <marcosgomesdev@gmail.com>
2024-06-15 17:07:06 -0300

    feat: add initial menu sheet

--- components/SideMenu/index.tsx
+++ components/SideMenu/index.tsx
@@ -1,10 +1,31 @@
 "use client";
+
 import { useSideMenu, useSideMenuService } from "@/services";
+import {
+  HeartIcon,
+  HomeIcon,
+  LogInIcon,
+  LogOutIcon,
+  ScrollTextIcon,
+} from "lucide-react";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Button } from "../ui/button";
+import { Separator } from "../ui/separator";
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from "../ui/sheet";
 
 export function SideMenu() {
   const menu = useSideMenu();
+  const { data } = useSession();
   const { hideSideMenu } = useSideMenuService();
+
+  function handleSignOutClick(): void {
+    signOut();
+  }
+
+  function handleSignInClick(): void {
+    signIn();
+  }
 
   return (
     <Sheet open={menu} onOpenChange={hideSideMenu}>
@@ -12,6 +33,79 @@ export function SideMenu() {
         <SheetHeader>
           <SheetTitle className="text-left">Menu</SheetTitle>
         </SheetHeader>
+
+        {data?.user ? (
+          <div className="flex justify-between pt-6">
+            <div className="flex items-center gap-3">
+              <Avatar>
+                <AvatarImage src={data?.user?.image as string | undefined} />
+                <AvatarFallback>
+                  {data?.user?.name?.charAt(0)}
+                  {data?.user?.name?.charAt(1)}
+                </AvatarFallback>
+              </Avatar>
+
+              <div>
+                <h3 className="font-semibold">{data?.user?.name}</h3>
+                <span className="block text-xs text-muted-foreground">
+                  {data?.user?.email}
+                </span>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="flex items-center justify-between pt-10">
+            <h2 className="font-semibold">Olá. Faça seu login!</h2>
+            <Button size="icon" onClick={handleSignInClick}>
+              <LogInIcon />
+            </Button>
+          </div>
+        )}
+
+        <Separator className="my-6" />
+
+        <div className="space-y-2">
+          <Button
+            variant="ghost"
+            className="w-full justify-start space-x-3 text-sm font-normal"
+          >
+            <HomeIcon size={16} />
+            <span className="block">Início</span>
+          </Button>
+
+          {data?.user && (
+            <>
+              <Button
+                variant="ghost"
+                className="w-full justify-start space-x-3 text-sm font-normal"
+              >
+                <ScrollTextIcon size={16} />
+                <span className="block">Meus Pedidos</span>
+              </Button>
+
+              <Button
+                variant="ghost"
+                className="w-full justify-start space-x-3 text-sm font-normal"
+              >
+                <HeartIcon size={16} />
+                <span className="block">Restaurantes Favoritos</span>
+              </Button>
+            </>
+          )}
+        </div>
+
+        <Separator className="my-6" />
+
+        {data?.user && (
+          <Button
+            variant="ghost"
+            className="w-full justify-start space-x-3 text-sm font-normal"
+            onClick={handleSignOutClick}
+          >
+            <LogOutIcon size={16} />
+            <span className="block">Sair da conta</span>
+          </Button>
+        )}
       </SheetContent>
     </Sheet>
   );
